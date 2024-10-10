ecf53b10191253d38ae3235c0631e53e2a320c15
KirillDemyanenko <demyanenkokg@gmail.com>
2024-10-10 15:54:42 +0300

    feat: implement solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,30 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    const arr = expr.split("")
+    let result = []
+    for (let i = 0; i < arr.length; i++) {
+        if (result.length) {
+            if (result[result.length - 1].length > 9) {
+                result.push([arr[i]])
+            } else {
+                result[result.length - 1].push(arr[i])
+            }
+        } else {
+            result.push([arr[i]])
+        }
+    }
+    result = result.map(item => {
+        if (item.includes('*')) {
+            return " "
+        }
+        const str = []
+        for (let j = 0; j < item.length; j = j + 2) {
+            str.push(item[j].concat(item[j + 1]).replace('10', '.').replace('11', '-'));
+        }
+        return str.includes('*') ? " " : MORSE_TABLE[str.join('').replaceAll('0', '')]
+    })
+    return result.join("")
 }
 
 module.exports = {
